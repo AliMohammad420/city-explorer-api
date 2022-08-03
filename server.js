61a00e36963969d4c9b8d55d7827f99a368c2953
AliMohammad420 <aliwessam509@gmail.com>
2022-08-03 17:50:13 +0300

    Lab09

--- server.js
+++ server.js
@@ -1,72 +1,23 @@
-'use strict'
-const axios = require('axios')
+"use strict";
+
+require("dotenv").config();
+const express = require("express");
 const cors = require("cors");
-const express = require('express');
+const axios = require("axios");
+const { handleWeather } = require("./modules/weather");
+const { handleMovies } = require("./modules/movies");
+
 const app = express();
-require('dotenv').config();
-const port = process.env.PORT || 3001
-
-
-app.listen(port, () => {
-    console.log(`Listening to  ${port}`)
-}
-)
 app.use(cors());
-app.get('/', (req, res) => {
-    res.send('Server Is UP!!!')
-}
-)
 
-app.get('/weather', async (req, res) => {
-    try {
-        let lat = req.query.lat;
-        let lon = req.query.lon;
-        let url = `https://api.weatherbit.io/v2.0/current?lat=${lat}&lon=${lon}&key=${process.env.WEATHER_API_KEY}`
-        let response = await axios.get(url)
-        let data = new Weather(response.data.data[0].datetime,response.data.data[0].weather.description)
-        res.send(data)
-        }
-    catch (error) {
-        res.status(400).send(error)
-    }
-}
-)
+app.get("/weather", handleWeather);
+app.get("/movies", handleMovies);
 
-class Weather {
-    constructor(datetime, description) {
-        this.date = datetime;
-        this.description = description;
-    }
-}
+app.get("*", (req, res) => {
+  res.status(404).send("Page not found");
+});
 
-
-
-app.get('/movies', async (req, res) => {
-    try {
-        let url = `https://api.themoviedb.org/3/search/movie?api_key=${process.env.MOVIE_API_KEY}&query=${req.query.title}`
-        let response = await axios.get(url)
-        let data = response.data.results.map(movie => new Movie(movie.title, movie.overview, movie.average_votes, movie.total_votes, movie.image_url, movie.popularity, movie.release_date))
-        res.send(data)
-        }
-    catch (error) {
-        res.status(400).send(error)
-    }
-}
-
-)
-
-
-class Movie {
-
-    constructor(title, overview, average_votes, total_votes, image_url, popularity, release_date) {
-        this.title = title;
-        this.overview = overview;
-
-        this.average_votes = average_votes;
-        this.total_votes = total_votes;
-        this.image_url = image_url;
-        
-        this.popularity = popularity;
-        this.release_date = release_date;
-    }
-}
+const PORT = process.env.PORT || 4000;
+app.listen(PORT, () => {
+  console.log(`Server listening on port ${PORT}`);
+});
